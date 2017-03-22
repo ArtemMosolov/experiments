b20b25bbbd3d3820ea1a5ddbb8bc46071602cdc3
Artem Mosolov <Artem_Mosolov@epam.com>
2017-03-22 13:59:59 +0200

    init spring boot demo project

---
+++ spring_experiments/src/main/java/com/spring/demo/SpringExperimentsApplication.java
@@ -0,0 +1,12 @@
+package com.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExperimentsApplication.class, args);
+	}
+}
